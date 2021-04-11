69576736d3181f7b78c433a241cac4c181ac3bb9
ahcock <63637115+ahcock@users.noreply.github.com>
2021-04-12 00:07:34 +0900

    Learn about Next Link component, and code splitting, client-side navigation, pre-fetching of Link Component

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+>
+pages/index.js,1/7/17f8c719689e4f3221289153187a6ca1fb24dd87
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function FirstPost() {
-    return <h1>First Post 무야호~~!</h1>
+    return (
+        <>
+        <h1>First Post 무야호~~!</h1>
+        <h2>
+            {/*<Link>*/}
+                <a href="/">Home으로 가자~!!</a>
+            {/*</Link>*/}
+        </h2>
+        </>
+    )
 }
