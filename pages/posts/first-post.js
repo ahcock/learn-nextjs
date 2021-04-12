856193a9a80b6bb69d2b042a9d53b13d6571a003
Park SangRok <josh@Parkui-MacBookPro.local>
2021-04-12 18:14:01 +0900

    Learn CSS styling in Next: CSS module, global CSS, Layout Component, Metadata manipulation, Assets.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,21 @@
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
-    return (
-        <>
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
         <h1>First Post 무야호~~!</h1>
         <h2>
-            {/*<Link>*/}
-                <a href="/">Home으로 가자~!!</a>
-            {/*</Link>*/}
+          <Link href="/">
+            <a>Home으로 가자~!!</a>
+          </Link>
         </h2>
-        </>
-    )
+      </Layout>
+    </>
+  );
 }
